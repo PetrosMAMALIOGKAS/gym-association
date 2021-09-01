9e9c97e4f2092b7f8752d136135eeddc0ac6dc34
Petros MAMALIOGKAS <petros.mamaliogas@gmail.com>
2021-09-01 10:24:06 +0200

    first commit

---
+++ src/main/java/com/simplon/gym/GymAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAssociationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplon/gym/GymAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
