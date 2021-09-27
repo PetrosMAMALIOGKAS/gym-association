45fa7c98fae518caf961f38dc5e88ca274068180
Petros MAMALIOGKAS <petros.mamaliogas@gmail.com>
2021-09-27 15:10:35 +0200

    model sportif and sports created

---
+++ src/main/java/model/Sports.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.List;
+
+public class Sports {
+	private List<String> jouer;
+	private List<String> arbitrer;
+	private List<String> entainer;
+	
+	
+	public Sports(List<String> jouer, List<String> arbitrer, List<String> entainer) {
+		super();
+		this.jouer = jouer;
+		this.arbitrer = arbitrer;
+		this.entainer = entainer;
+	}
+
+
+	public List<String> getJouer() {
+		return jouer;
+	}
+
+	public void setJouer(List<String> jouer) {
+		this.jouer = jouer;
+	}
+
+	public List<String> getArbitrer() {
+		return arbitrer;
+	}
+
+	public void setArbitrer(List<String> arbitrer) {
+		this.arbitrer = arbitrer;
+	}
+
+	public List<String> getEntainer() {
+		return entainer;
+	}
+
+	public void setEntainer(List<String> entainer) {
+		this.entainer = entainer;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Sports [jouer=" + jouer + ", arbitrer=" + arbitrer + ", entainer=" + entainer + "]";
+	}
+	
+	
+	
+
+}
